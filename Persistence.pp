cb5515bdcca8a1f99a450a3d3bd3be621c7b13fd
Carlos Roque <carlosroque198@gmail.com>
2021-07-17 19:13:47 -0400

    First test passed

---
+++ Persistence.pp
@@ -0,0 +1,36 @@
+{$mode objfpc}{$H+}
+
+unit Persistence;
+
+interface
+
+type
+  TPersistence = class
+  public
+    function Persistence(Num: Integer): Integer;
+  end;
+  
+implementation
+
+function TPersistence.Persistence(Num: Integer): Integer;
+var
+  NumberOfTimes: Integer;
+  Temp: Integer;
+begin
+  NumberOfTimes:= 0;
+  while Num > 9 do
+  begin
+    Temp:= 1;
+    while Num > 0 do
+	begin
+	  Temp:= Temp * (Num mod 10);
+      Num := Num div 10;	  
+	end;
+	Num:= Temp;
+	NumberOfTimes:= NumberOfTimes + 1;
+  end;
+  
+  Result:= NumberOfTimes;
+end;
+
+end.
---
+++ Requirements.md
@@ -0,0 +1,16 @@
+# Requirements
+
+- The program must be able to return me the multiplicative persistence of a given integer.
+
+# Acceptance Criteria
+
+- The program accepts a integer and returns another integer.
+- The returned value matches the multiplicative persistence of that given number.  
+
+# Scenarios
+
+- 5 => 0 
+-  198 => 3
+-  -198 => 3
+-  15961 => 2
+-  796 => 5
